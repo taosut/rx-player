59fa43fe8c1d8fb19f3d62c1ff3953569dbcbad4
grenault73 <grenault73@gmail.com>
2018-06-13 15:12:45 +0200

    parsers: add emsg parser

--- src/parsers/containers/isobmff/index.ts
+++ src/parsers/containers/isobmff/index.ts
@@ -23,14 +23,26 @@ import {
   concat,
   hexToBytes,
   itobe4,
+  readTerminatedString,
   strToBytes,
 } from "../../../utils/bytes";
 import { getPlayReadyKIDFromPrivateData } from "./drm";
 import {
+  getEMSG,
   getMDAT,
   getMDIA,
   getTRAF,
 } from "./read";
+
+interface IEMSG {
+  schemeId: string;
+  value: string;
+  timescale: number;
+  presentationTimeDelta: number;
+  eventDuration: number;
+  id: number;
+  messageData: Uint8Array;
+}
 
 interface IISOBMFFKeySystem {
   systemId : string;
@@ -417,6 +429,52 @@ function patchPssh(buf : Uint8Array, pssList : IISOBMFFKeySystem[]) : Uint8Array
   );
 }
 
+/**
+ * Parse EMSG box from ISOBMFF data.
+ * @param {Uint8Array} buf
+ * @returns {Object|null}
+ */
+function parseEmsg(buf: Uint8Array) : IEMSG|undefined {
+  const emsg = getEMSG(buf);
+  if (emsg === null) {
+    return undefined;
+  }
+
+  const length = emsg.length;
+
+  let position = 4; // skip version + flags
+
+  const { end: schemeIdEnd, string: schemeId } = readTerminatedString(emsg, position);
+  position = schemeIdEnd; // skip schemeId
+
+  const { end: valueEnd, string: value } = readTerminatedString(emsg, position);
+  position = valueEnd; // skip value
+
+  const timescale = be4toi(emsg, position);
+  position += 4; // skip timescale
+
+  const presentationTimeDelta = be4toi(emsg, position);
+  position += 4; // skip presentationTimeDelta
+
+  const eventDuration = be4toi(emsg, position);
+  position += 4; // skip eventDuration
+
+  const id = be4toi(emsg, position);
+  position += 4; // skip id
+
+  const messageData = emsg.subarray(position, length);
+
+  return {
+    schemeId,
+    value,
+    timescale,
+    presentationTimeDelta,
+    eventDuration,
+    id,
+    messageData,
+  };
+}
+
 export {
   getMDHDTimescale,
   getPlayReadyKIDFromPrivateData,
@@ -427,4 +485,5 @@ export {
   getMDIA,
   getTRAF,
   patchPssh,
+  parseEmsg
 };
--- src/utils/bytes.ts
+++ src/utils/bytes.ts
@@ -379,6 +379,29 @@ function toBase64URL(str : string) : string {
   return btoa(str).replace(/\=+$/, "");
 }
 
+/**
+ * Decode string from bytes (UTF-8).
+ * Keeps reading until it reaches a byte that equals to zero.
+ * @param {Uint8Array} buf
+ * @param {number} offset
+ */
+function readTerminatedString(buf: Uint8Array, offset: number) {
+  let position = offset;
+  while (position < buf.length) {
+    const value = buf[position];
+    if (value === 0) {
+      break;
+    }
+    position += 1;
+  }
+
+  const bytes = new Uint8Array(buf.buffer, offset, position - offset);
+  return {
+    end: position + 1,
+    string: new TextDecoder().decode(bytes),
+  };
+}
+
 export {
   strToBytes,
   bytesToStr, bytesToUTF16Str,
@@ -391,4 +414,5 @@ export {
   itole2, itole4, itole8,
   guidToUuid,
   toBase64URL,
+  readTerminatedString
 };
